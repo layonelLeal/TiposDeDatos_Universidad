6e078f1393f80df7484357e1c66534925bb55a15
layonel <layonel@LAPTOP-A6QNOGJ4>
2024-02-21 20:57:16 -0500

    First themplate project and friends

--- src/tiposdedatos/TiposDeDatos.java
+++ src/tiposdedatos/TiposDeDatos.java
@@ -11,43 +11,39 @@ import java.util.Scanner;
  * El siguiente código se encarga de pedir por consola
  * cada uno de los tipos de datos primitivos. Mostrando lo que ingresó el
  * usuario y que peso tiene en memoria.
- */ 
+ */
+
 public class TiposDeDatos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         final int INT_SIZE = Integer.SIZE;
         final int BOOLEAN_SIZE = 1;
         final int CHARACTER_SIZE = Character.SIZE;
         final int BYTE_SIZE = Byte.SIZE;
+        final int LONG_SIZE = Long.SIZE;
+        final int SHORT_SIZE = Short.SIZE;
+        final int FLOAT_SIZE = Float.SIZE;
+        final int DOUBLE_SIZE = Double.SIZE;
+
         String input;
         
+       
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa un boleano: ");
-        //boolean user_boleano = Boolean.parseBoolean();
-        input = sc.next();
-        
-        if (input.contains("true") || input.contains("false") || input.contains("0") || input.contains("1")){
-            System.out.println("Si es un booleano");
-        }else{
-            System.out.println("No es un booleano, por favor ingrese nuevamente el valor.");
-        }
-        
-        boolean isBolean = Boolean.parseBoolean(input);
-        if (!isBolean){
-            System.out.println("No es un booleano");
-        }
-        
-        //System.out.println("El Boleano Igresado es: " + user_boleano + " y ocupa un espacio de: " + BOOLEAN_SIZE + " bits");
+        boolean user_boleano = Boolean.parseBoolean(sc.next()); //Aca estoy cambiando de valor un string a un valor boleano.
+        System.out.println("El Boleano Igresado es: " + user_boleano + " y ocupa un espacio de: " + BOOLEAN_SIZE + " bits");
         
         System.out.println("Ingresa un caracter: ");
         char user_caracter = sc.next().charAt(0);
+        
         System.out.println("El Caracter Igresado es: " + user_caracter + " y ocupa un espacio de: " + CHARACTER_SIZE + " bits");
         System.out.println("Ingresa un byte: ");
-        byte user_byte = Byte.parseByte(sc.next());
+        byte user_byte = sc.nextByte();
         System.out.println("El Caracter Igresado es: " + user_byte + " y ocupa un espacio de: " + BYTE_SIZE + " bits");
+        
+        System.out.println("Ingresa un numero entero: ");
+        int user_entero = sc.nextInt();
+        System.out.println("El numero entero ingresado es: " + user_entero + " y ocupa un espacio de: "+ INT_SIZE + " bits");
         
     }
 
