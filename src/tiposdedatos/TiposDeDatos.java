5384471f353e53c51201d6cc659d819b890e66c3
layonel <layonel@LAPTOP-A6QNOGJ4>
2024-02-21 21:49:53 -0500

    Finish sample demo

--- src/tiposdedatos/TiposDeDatos.java
+++ src/tiposdedatos/TiposDeDatos.java
@@ -24,27 +24,45 @@ public class TiposDeDatos {
         final int SHORT_SIZE = Short.SIZE;
         final int FLOAT_SIZE = Float.SIZE;
         final int DOUBLE_SIZE = Double.SIZE;
-
-        String input;
-        
        
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("|------ BIENVENIDO ------|");
+        System.out.println("Ejercicio Practico de los tipos de datos.");
+        
         System.out.println("Ingresa un boleano: ");
         boolean user_boleano = Boolean.parseBoolean(sc.next()); //Aca estoy cambiando de valor un string a un valor boleano.
         System.out.println("El Boleano Igresado es: " + user_boleano + " y ocupa un espacio de: " + BOOLEAN_SIZE + " bits");
         
         System.out.println("Ingresa un caracter: ");
-        char user_caracter = sc.next().charAt(0);
+        char user_character = sc.next().charAt(0);
+        System.out.println("El Caracter Igresado es: " + user_character + " y ocupa un espacio de: " + CHARACTER_SIZE + " bits");
         
-        System.out.println("El Caracter Igresado es: " + user_caracter + " y ocupa un espacio de: " + CHARACTER_SIZE + " bits");
         System.out.println("Ingresa un byte: ");
         byte user_byte = sc.nextByte();
-        System.out.println("El Caracter Igresado es: " + user_byte + " y ocupa un espacio de: " + BYTE_SIZE + " bits");
+        System.out.println("El Byte Igresado es: " + user_byte + " y ocupa un espacio de: " + BYTE_SIZE + " bits");
         
-        System.out.println("Ingresa un numero entero: ");
-        int user_entero = sc.nextInt();
-        System.out.println("El numero entero ingresado es: " + user_entero + " y ocupa un espacio de: "+ INT_SIZE + " bits");
+        System.out.println("Ingresa un numero del tipo Short: ");
+        short user_short = sc.nextShort();
+        System.out.println("El numero Short ingresado es: " + user_short + " y ocupa un espacio de: "+ SHORT_SIZE + " bits");
         
+        System.out.println("Ingresa un numero del tipo Int: ");
+        int user_int = sc.nextInt();
+        System.out.println("El numero Int ingresado es: " + user_int + " y ocupa un espacio de: "+ INT_SIZE + " bits");
+        
+        System.out.println("Ingresa un numero del tipo Long: ");
+        long user_long = sc.nextLong();
+        System.out.println("El numero Long ingresado es: " + user_long + " y ocupa un espacio de: "+ LONG_SIZE + " bits");
+        
+        System.out.println("Ingresa un numero del tipo Float: ");
+        float user_float = sc.nextFloat();
+        System.out.println("El numero Long ingresado es: " + user_float + " y ocupa un espacio de: "+ FLOAT_SIZE + " bits");
+        
+        System.out.println("Ingresa un numero del tipo Double: ");
+        double user_double = sc.nextDouble();
+        System.out.println("El numero Double ingresado es: " + user_double + " y ocupa un espacio de: "+ DOUBLE_SIZE + " bits");
+        
+        sc.close();
     }
 
 }
