61c5c4ba0f10e67210676d8d627cb3de2f3e2b57
layonel <layonel@LAPTOP-A6QNOGJ4>
2024-02-21 20:26:06 -0500

    First themplate project

--- src/tiposdedatos/TiposDeDatos.java
+++ src/tiposdedatos/TiposDeDatos.java
@@ -3,11 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tiposdedatos;
+import java.util.Scanner;
 
 /**
  *
  * @author layonel Camilo Leal Diaz
- * Tipos de datos
+ * El siguiente código se encarga de pedir por consola
+ * cada uno de los tipos de datos primitivos. Mostrando lo que ingresó el
+ * usuario y que peso tiene en memoria.
  */ 
 public class TiposDeDatos {
 
@@ -15,7 +18,37 @@ public class TiposDeDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final int INT_SIZE = Integer.SIZE;
+        final int BOOLEAN_SIZE = 1;
+        final int CHARACTER_SIZE = Character.SIZE;
+        final int BYTE_SIZE = Byte.SIZE;
+        String input;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa un boleano: ");
+        //boolean user_boleano = Boolean.parseBoolean();
+        input = sc.next();
+        
+        if (input.contains("true") || input.contains("false") || input.contains("0") || input.contains("1")){
+            System.out.println("Si es un booleano");
+        }else{
+            System.out.println("No es un booleano, por favor ingrese nuevamente el valor.");
+        }
+        
+        boolean isBolean = Boolean.parseBoolean(input);
+        if (!isBolean){
+            System.out.println("No es un booleano");
+        }
+        
+        //System.out.println("El Boleano Igresado es: " + user_boleano + " y ocupa un espacio de: " + BOOLEAN_SIZE + " bits");
+        
+        System.out.println("Ingresa un caracter: ");
+        char user_caracter = sc.next().charAt(0);
+        System.out.println("El Caracter Igresado es: " + user_caracter + " y ocupa un espacio de: " + CHARACTER_SIZE + " bits");
+        System.out.println("Ingresa un byte: ");
+        byte user_byte = Byte.parseByte(sc.next());
+        System.out.println("El Caracter Igresado es: " + user_byte + " y ocupa un espacio de: " + BYTE_SIZE + " bits");
+        
     }
-    
+
 }
